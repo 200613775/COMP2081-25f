5ab1ecd5cae2483d5a75d2a4ee184a3813b435a2
200613775 <patelnishi369@gmail.com>
2025-11-24 16:48:36 -0500

    Added a little bit js for smooth scroll navbar links

--- Assignment-2/js/script.js
+++ Assignment-2/js/script.js
@@ -1,2 +1,13 @@
+// Smooth scroll for navbar links
+document.querySelectorAll('a.nav-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        const target = document.querySelector(this.getAttribute('href'));
+        if(target) {
+            e.preventDefault();
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
+
 // js file added
 console.log("Custom JS file loaded.");
